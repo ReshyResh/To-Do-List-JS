6d45a534517d528b71d8cf4d86673fc9cf6348cf
Reshy <reshy@protonmail.com>
2021-08-23 22:52:15 +0200

    Change swap function to use destructuring

--- src/swap.js
+++ src/swap.js
@@ -1,12 +1,9 @@
+/* eslint max-len: 0 */
 import { setStorage } from './storage';
 
 export default (arr, index1, index2) => {
-  const temp1 = arr[index1].description;
-  const temp2 = arr[index1].completed;
-  arr[index1].description = arr[index2].description;
-  arr[index1].completed = arr[index2].completed;
-  arr[index2].description = temp1;
-  arr[index2].completed = temp2;
+  [arr[index1].description, arr[index2].description] = [arr[index2].description, arr[index1].description];
+  [arr[index1].completed, arr[index2].completed] = [arr[index2].completed, arr[index1].completed];
   setStorage(arr);
   return arr;
 };
