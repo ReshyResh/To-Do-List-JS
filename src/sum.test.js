6f28d6d428184fee3c1206cf3156cfdb6ce3afd7
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-08-25 18:00:04 +0300

    add test for edit and checkbox function

--- src/sum.test.js
+++ src/sum.test.js
@@ -7,39 +7,77 @@ import removeSplice from './remove-one';
 import editItem from './edit-item';
 import checkbox from './checkboxes';
 import swap from './swap';
-import { setStorage } from './storage';
+import {
+    setStorage
+} from './storage';
 import clear from './remove-item';
 
 describe('Add/Remove', () => {
-  test('Add one new item to the list', () => {
-    const arr = [
-      {
-        description: 'Task 1',
-        completed: false,
-        index: 1,
-      },
-    ];
-    document.body.innerHTML = '<input id="input-text" value="Item added for test purposes">';
-    add(arr);
-    add(arr);
-    add(arr);
-    add(arr);
-    expect(arr).toHaveLength(5);
-  });
-  test('Remove one item from the list', () => {
-    const arr = [
-      {
-        description: 'Task 1',
-        completed: false,
-        index: 0,
-      },
-      {
-        description: 'Task 2',
-        completed: false,
-        index: 1,
-      },
-    ];
-    removeSplice(arr, 0);
-    expect(arr).toHaveLength(1);
-  });
+    test('Add one new item to the list', () => {
+        const arr = [{
+            description: 'Task 1',
+            completed: false,
+            index: 1,
+        }, ];
+        document.body.innerHTML = '<input id="input-text" value="Item added for test purposes">';
+        add(arr);
+        add(arr);
+        add(arr);
+        add(arr);
+        expect(arr).toHaveLength(5);
+    });
+    test('Remove one item from the list', () => {
+        const arr = [{
+                description: 'Task 1',
+                completed: false,
+                index: 0,
+            },
+            {
+                description: 'Task 2',
+                completed: false,
+                index: 1,
+            },
+        ];
+        removeSplice(arr, 0);
+        expect(arr).toHaveLength(1);
+    }); 
+});
+
+describe('Edit/Checkbox/Swap/Clear', () => {
+    test('Edit a task description', () => {
+        document.body.innerHTML ='<input id="change0" value="This is an edit of task at index 0">'
+        const arr = [
+            {
+                description: 'Task 1',
+                completed: false,
+                index: 0,
+            },
+            {
+                description: 'Task 2',
+                completed: false,
+                index: 1,
+            }
+        ];
+        editItem(arr,0);
+        expect(arr[0].description).toBe("This is an edit of task at index 0");
+    });
+
+    test('Update completed status based on checkbox', () => {
+        document.body.innerHTML ='<li id="item0"><input type="checkbox" checked id="0"><li id="item1"><input type="checkbox" id="1">'
+        const arr = [
+            {
+                description: 'Task 1',
+                completed: false,
+                index: 0,
+            },
+            {
+                description: 'Task 2',
+                completed: false,
+                index: 1,
+            }
+        ];
+
+        checkbox(arr);
+        expect(arr[0].completed).toBe("true");
+    });
 });
